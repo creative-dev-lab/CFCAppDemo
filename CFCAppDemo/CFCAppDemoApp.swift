77252d524c01bc9d7f7ed074d4d7dffc88509647
Eli Zhang <elizhang221@gmail.com>
2023-02-17 04:04:52 -0500

    Initial Commit

---
+++ CFCAppDemo/CFCAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CFCAppDemoApp.swift
+//  CFCAppDemo
+//
+//  Created by King on 2/17/23.
+//
+
+import SwiftUI
+
+@main
+struct CFCAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
